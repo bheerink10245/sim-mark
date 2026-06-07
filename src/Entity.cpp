d34c99bb612818fb9119cd53fd09659a48d32fd8
Bryce Heerink <bryceheerink1@gmail.com>
2026-06-07 02:33:48 -0400

    hbiuok

---
+++ src/Entity.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Asset.cpp"
+
+class Entity {
+public:
+
+
+};
+
+namespace Entities
+
+
+class Market_Maker : public Entity {
+public:
+    Market_Maker(std::string& u_name)
+
+
+};
+
+
+class Individual : public Entity {
+private:
+    long long double cash_;
+    std::string name_;
+    std::vector<Ticker> positions_;
+    std::vector<Trade> tradeLog_;
+
+public: 
+    Individual(std::string& name, long long double& cash) {
+
+
+    }
+
+};
